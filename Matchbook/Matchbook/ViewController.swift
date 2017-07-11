8801d026ea3b6ae7c614a7b7323af41389fa6591
Haejin <haejinjo@g.ucla.edu>
2017-07-11 15:41:26 -0700

    installed alamofire and swifty pods. also this is the last working version of our efforts to manually constrain everything in main VC without stack views

--- Matchbook/Matchbook/ViewController.swift
+++ Matchbook/Matchbook/ViewController.swift
@@ -7,18 +7,51 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    // VC LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+// IB OUTLETS
+    @IBOutlet weak var profileButton: UIButton!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    @IBOutlet weak var coverImage: UIImageView!
+    
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    @IBOutlet weak var buyButton: UIButton!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+
+// IB ACTIONS
+    @IBAction func buyButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func profileButtonTapped(_ sender: UIButton) {
+    }
+
+
 
 
 }
